738c22cfd1f0579ea0f301d6a679b34d73d2e93c
Tainara17 <tainaramartinscarvalho@gmail.com>
2021-08-09 19:37:28 -0300

    teste operações doubleArrayOf parte 2

--- src/one/digitalinnovation/colections/TesteOperações.kt
+++ src/one/digitalinnovation/colections/TesteOperações.kt
@@ -14,8 +14,17 @@ fun main () {
 
     val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
     println ("..............................")
+
     salariosMaiorQue2500.forEach { print(it) }
 
+    println ("..............................")
+    println(salarios.count { it in 2000.0..5000.00 })
 
+    println ("..............................")
+    println(salarios.find { it == 2000.0 })
+    println(salarios.find { it == 5000.0 }) // o que acontece quando procura um que não existe
 
+    println ("..............................")
+    println(salarios.any { it == 1000.0 }) //elemento em que a coleção seja verdadeira
+    println(salarios.any { it == 7000.0 }) //elemento em que a coleção seja falsa
 }
